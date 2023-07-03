f097063b7a97dfb6a74b33bea539215c19f07d7a
Citral87 <thierry.salzedas@gmail.com>
2023-07-03 21:53:45 +0200

    up
    up

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     content: ["index.html", "assets/scripts.js"],
     css: ["assets/bootstrap/bootstrap.min.css"],
+    safelist: ['gallery'],
     output: "assets/dist",
     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
   };
