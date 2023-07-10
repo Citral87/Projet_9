d0f28efb2ef1e41854e5da1f52f23128a901d273
Citral87 <thierry.salzedas@gmail.com>
2023-07-10 13:14:59 +0200

    maj
    maj

--- purgecss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ["index.html", "assets/scripts.js"],
-    css: ["assets/bootstrap/bootstrap.min.css"],
-    safelist: ['gallery'],
-    output: "assets/dist",
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  };
-  
